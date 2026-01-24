5325a651ff998156c69e733812daa958cf6994e4
fabiorodriguesroque <fabioroque9@gmail.com>
2026-01-24 17:46:57 +0000

    wip

--- src/pages/[level]/page.tsx
+++ src/pages/[level]/page.tsx
@@ -1,25 +1,38 @@
 'use client'
 import { images } from '@repo/domino/constants/images'
 import { useAws } from '@repo/core/hooks'
-import tiles from '../../constants/tiles'
-import DominoTile from '../../components/domino-tile'
+import TopBar from '@repo/ui/components/games/top-bar'
 
 export default function GamePage() {
   const { getPublicUrl } = useAws()
 
   return (
-    <div
-      className="font-poppins h-screen w-screen"
-      style={{
-        backgroundImage: `url(${getPublicUrl(images.ui.background)})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
-      <div className="flex flex-wrap gap-2">
-        {tiles.map((tile) => (
-          <DominoTile key={tile.id} tile={tile} />
-        ))}
+    <div className="font-poppins relative h-screen w-screen bg-[#9AD7CE]">
+      {/* Background image with opacity */}
+      <div
+        className="absolute inset-0 opacity-50"
+        style={{
+          backgroundImage: `url(${getPublicUrl(images.ui.background)})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      {/* Content */}
+      <div className="relative z-10 h-full">
+        <div className="fixed top-10 w-full">
+          <div className="mx-auto max-w-3xl">
+            <TopBar
+              stars={0}
+              maxStars={5}
+              livesPercentage={100}
+              onBack={() => {}}
+              onHelp={() => {}}
+            />
+          </div>
+        </div>
+        <div className="flex h-full flex-1 items-center justify-center">
+          Game Zone
+        </div>
       </div>
     </div>
   )
